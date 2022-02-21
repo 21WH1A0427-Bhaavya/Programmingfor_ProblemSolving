15fb2a4c5bf43e963441c5a9b8565429f41c8787
21WH1A0427-Bhaavya <98684290+21WH1A0427-Bhaavya@users.noreply.github.com>
2022-02-21 12:03:56 +0530

    Delete sizeof.cpp

--- sizeof.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{ printf("CALCULATE THE SIZE OF DATA TYPES:\n\n");
-printf("SIZE OF CHAR DATA TYPE IS %ld",sizeof(char));
-printf("\nSIZE OF INT DATA TYPE IS %ld",sizeof(int));
-printf("\nSIZE OF FLOAT DATA TYPE IS %ld",sizeof(float));
-printf("\nSIZE OF DOULBLE DATA TYPE IS %ld",sizeof(double));
-}
